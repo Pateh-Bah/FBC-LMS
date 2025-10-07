4453878f9239ae2e28a7df722da9bb5a6dc945f3
Pateh-bah <ibrahimpatehbah@gmail.com>
2025-10-07 17:25:14 +0000

    vercel and database connection sync

--- library_system/wsgi.py
+++ library_system/wsgi.py
@@ -12,7 +12,7 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 from pathlib import Path
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_system.settings_vercel')
 
 # Get base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
